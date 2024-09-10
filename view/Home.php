a223b38a30235a6a7fa88ad28df384d0f45353e7
hsazzad-prog <hsazzad02@gmail.com>
2024-09-10 18:37:53 +0600

    CSS Task

---
+++ view/Home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   
+    <title>Banking Website</title>
+    <link rel="stylesheet" href="styles.css"> 
+    
+</head>
+
+<body>
+    <div class="logo">BankPro</div>
+  
+    <div class="sticky">
+        <table>
+            <tr>
+                <td><a href="#">Home</a></td>
+                    <td><a href="#">About Us</a></td>
+                    <td><a href="#">Services</a></td>
+                    <td><a href="#">Contact Us</a></td>
+                    <td><a href="#">Login</a></td>
+            </tr>
+    </table>
+    </div>
+  
+    
+<div class="relative">
+            <h1>Welcome to BankPro</h1>
+            <p>Your trusted partner for financial solutions</p>
+            <button class="button button1">Sign Up</button>
+            <button class="button button2">About US</button>
+</div>
+          
+      
+
+
+    <div class="fixed">
+        <p>&copy; 2024 BankPro. All Rights Reserved.</p>
+    </div>
+
+</body>
+
+</html>
